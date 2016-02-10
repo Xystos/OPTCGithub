5dcbc00a72d9d1556a4a30ceede66c3e8677490a
Xystos <jacksofspadess@Gmail.com>
2016-02-10 11:08:59 -0800

    Update units.js

--- common/data/units.js
+++ common/data/units.js
@@ -1,5 +1,6 @@
 window.units = [
 	[ "Monkey D. Luffy", "STR", "Fighter", 2, 1, 5, 0, 5, 163, 42, 15, 8, 134, 68, 15, 1 ],
+	["Sengoku, The Only Marine", "PSY", "Knowledge", "Tough", 6, 55, 4, 99, 3000000, 1600, 563, -200, 3200, 2000, 600, 1],
 	[ "Monkey D. Luffy Gum-Gum Pistol", "STR", "Fighter", 3, 4, 6, 0, 20, 26184, 130, 76, 14, 510, 249, 49, 0.7 ],
 	[ "Monkey D. Luffy Gum-Gum Bazooka", "STR", "Fighter", 4, 7, 6, 2, 35, 136167, 404, 227, 39, 975, 522, 105, 1 ],
 	[ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Freedom" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
